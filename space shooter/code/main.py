13a053b8c13255a26ae585d534dea7ff420753a9
beniman8 <benik@live.ca>
2025-02-17 19:48:35 -0500

    part three

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -7,22 +7,34 @@ WINDOW_WIDTH , WINDOW_HEIGHT = 1280 ,720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption('Spaceship Game')
 running = True
+clock = pygame.time.Clock()
 
 
 # plain Surface 
 surf = pygame.Surface((100,200))
 surf.fill('red')
 
-# import an image 
+# imports
 path = join('images','player.png')
 star_path = join('images','star.png')
+meteor_path = join('images','meteor.png')
+laser_path = join('images','laser.png')
 
-player_surf = pygame.image.load(path).convert_alpha()
+
+meteor_surf = pygame.image.load(meteor_path).convert_alpha()
+laser_surf = pygame.image.load(laser_path).convert_alpha()
 star_surf = pygame.image.load(star_path).convert_alpha()
-star_positions = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
-x = 100
+player_surf = pygame.image.load(path).convert_alpha()
 
+player_rect = player_surf.get_frect(center= (WINDOW_WIDTH / 2,WINDOW_HEIGHT/2))
+meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2,WINDOW_HEIGHT/2))
+laser_rect = laser_surf.get_frect(bottomleft = (20,WINDOW_HEIGHT - 20))
+
+star_positions = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
+
+player_direction = 1
 while running:
+    clock.tick(60)
     # event loop 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,12 +42,24 @@ while running:
 
     # draw the game  
     display_surface.fill('dark grey')
-    x += 0.1
+    #x += 0.1
     
     for pos in star_positions:
         display_surface.blit(star_surf,pos)
     
-    display_surface.blit(player_surf,(x,150))
+    
+    
+    display_surface.blit(laser_surf,laser_rect)
+    display_surface.blit(meteor_surf,meteor_rect)
+    
+    
+    player_rect.x += player_direction *0.8
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf,player_rect)
+    
+
+    
     pygame.display.update()
 
 pygame.quit()

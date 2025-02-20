52504937884163f12e8e54f295a00228261b9cbf
beniman8 <benik@live.ca>
2025-02-20 05:27:04 -0500

    getting input

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -32,16 +32,35 @@ laser_rect = laser_surf.get_frect(bottomleft = (20,WINDOW_HEIGHT - 20))
 
 star_positions = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
 
-player_direction = pygame.math.Vector2(1,0)
+player_direction = pygame.math.Vector2()
 player_speed = 300
 while running:
-    dt = clock.tick(60) / 1000
+    dt = clock.tick() / 1000
     
     # event loop 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #     print(1)
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
 
+    #input / controls
+    
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT] - keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN] - keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
+    
+    
+    # space
+    recent_key = pygame.key.get_just_pressed()
+    if recent_key[pygame.K_SPACE]:
+        print('fire laser')
+    
+    
     # draw the game  
     display_surface.fill('dark grey')
     #x += 0.1
@@ -56,6 +75,9 @@ while running:
     
     
     player_rect.center += player_direction * player_speed * dt
+    
+
+    
     display_surface.blit(player_surf,player_rect)
     
 

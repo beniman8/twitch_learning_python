07220e49d0786507a729e1a7ec268459ec4302d0
beniman8 <benik@live.ca>
2025-03-11 05:19:40 -0400

    game finish

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -2,10 +2,10 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
-
+from enemy import *
 from groups import AllSprites
 
-from random import randint
+from random import randint,choice
 
 
 class Game:
@@ -25,14 +25,55 @@ class Game:
         # Groups
         self.all_sprite = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
+        self.enemy_sprites = pygame.sprite.Group()
         
+        
+        
+
+        #gun timer 
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
+        
+        #enemy timer 
+        self.enemy_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.enemy_event,500)
+        self.spawn_positions = []
+        
+        # audio
+        self.shoot_sound= pygame.mixer.Sound(join('audio','shoot.wav'))
+        self.shoot_sound.set_volume(0.2)
+        
+        self.impact_sound= pygame.mixer.Sound(join('audio','impact.ogg'))
+        self.impact_sound.set_volume(0.2)
+        
+        
+        self.music= pygame.mixer.Sound(join('audio','music.wav'))
+        self.music.set_volume(0.05)
+        self.music.play(loops=-1)
+        
+        # setup
+        self.load_images()
         self.load_map()
-
-        #sprites
-
+        
+        
+        
         # run the game
         self.run_game()
         
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('images','gun','bullet.png')).convert_alpha()
+        folders  = list(walk(join('images','enemies')))[0][1]
+        self.enemy_frames = {}
+        for folder in folders:
+            for folder_path,_,files_names  in walk(join('images','enemies',folder)):
+                self.enemy_frames[folder] = []
+                for file_name in sorted(files_names,key = lambda name: int(name.split('.')[0])):
+                    full_path = join(folder_path,file_name)
+                    surf = pygame.image.load(full_path).convert_alpha()
+                    self.enemy_frames[folder].append(surf)
+                    
     def load_map(self):
         ''' Load the map and the items in the map'''
         
@@ -50,8 +91,43 @@ class Game:
             if obj.name == "Player":
                 # create the player only when you find the entity named Player
                 self.player = Player(self.player_surf,(obj.x,obj.y),self.all_sprite,self.collision_sprites)
+                self.gun = Gun(self.player,self.all_sprite)
+            else:
+                self.spawn_positions.append((obj.x,obj.y))
+                
+    def bullet_collision(self):
+        if self.bullet_sprites:
+            for bullet in self.bullet_sprites:
+                collision_sprites = pygame.sprite.spritecollide(bullet,self.enemy_sprites,False,pygame.sprite.collide_mask)
+                if collision_sprites:
+                    for sprite in collision_sprites:
+                        sprite.destroy()
+                        self.impact_sound.play()
+                    bullet.kill()
+                    
+    def player_collision(self):
+        if pygame.sprite.spritecollide(self.player,self.enemy_sprites,False,pygame.sprite.collide_mask):
+            self.running = False
         
-
+        
+    def input(self):
+        '''checking for game inputs'''
+        #left mouse button is clicked and the shooting is available
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            self.shoot_sound.play()
+            Bullet(self.bullet_surf,pos,self.gun.player_direction,(self.all_sprite,self.bullet_sprites))
+            self.can_shoot = False 
+            self.shoot_time = pygame.time.get_ticks()
+            
+    def gun_timer(self):
+        '''resetting the gun timer'''
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True 
+            
+            
     def run_game(self):
         '''Game loop : run the game loop'''
 
@@ -62,9 +138,16 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running= False
-
+                if event.type == self.enemy_event:
+                    Enemy(choice(self.spawn_positions),choice(list(self.enemy_frames.values())),(self.all_sprite,self.enemy_sprites),self.player,self.collision_sprites)
+                
+            
             #update all sprites
+            self.gun_timer()
+            self.input()
             self.all_sprite.update(dt)
+            self.bullet_collision()
+            self.player_collision()
 
 
             # draw the game
@@ -72,6 +155,7 @@ class Game:
             self.display_surface.fill("black")
             self.all_sprite.draw(self.player.rect.center)
 
+            #updating game
             pygame.display.update()
 
 
--- Vampire survivor/code/player.py
+++ Vampire survivor/code/player.py
@@ -1,4 +1,5 @@
 from settings import *
+from groups import *
 
 class Player(pygame.sprite.Sprite):
     
@@ -6,25 +7,41 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self,surf,pos, groups,collision_sprite):
         super().__init__(groups)
+        self.load_images()
+        self.state , self.frame_index ='down',0
         self.image = surf
         self.rect = self.image.get_frect(center=pos)
         self.hitbox_rect = self.rect.inflate(-60,-90)
         
+        
         # movement
         self.direction = pygame.Vector2()
-        self.speed = 300
+        self.speed = 500
         
         self.collision_sprite = collision_sprite
+        
+    def load_images(self):
+        self.frames = {'left':[],'right':[],'up':[],'down':[]}
+        
+        for state in self.frames.keys():
+            for folder_path,sub_folders,file_names in list(walk(join('images','player',state))):
+                if file_names:
+                    #sorted the list for safe guard incase the images do not show up in their respective orders
+                    for file_name in sorted(file_names,key= lambda name: int(name.split('.')[0])):
+                        full_path = join(folder_path,file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
         
         
     def input(self):
         '''The mechanics or inputs we are going to use in order to manipulate the player'''
         keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_RIGHT] - keys[pygame.K_LEFT])
-        self.direction.y = int(keys[pygame.K_DOWN] - keys[pygame.K_UP])
+        self.direction.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]  - keys[pygame.K_LEFT] or keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s] - keys[pygame.K_UP] or keys[pygame.K_w])
         self.direction = (
             self.direction.normalize() if self.direction else self.direction
-        )     
+        )
+
     
     def move(self,dt):
         ''' the actual movement that happens over time depending on the speed ,input and delta time'''
@@ -44,14 +61,26 @@ class Player(pygame.sprite.Sprite):
             if sprite.rect.colliderect(self.hitbox_rect):
                 if direction == 'horizontal':
                     if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
-                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+                    elif self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
                 else:
                     if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
-                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
-
+                    elif self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
+    
+    def animate(self,dt):
+        #get the state 
+        if self.direction.x != 0:
+            self.state='right' if self.direction.x > 0 else 'left'
+        if self.direction.y != 0:
+            self.state='down' if self.direction.y > 0 else 'up'
+        
+        #this needs to be studied or understood clear time 5:19
+        #animation 
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0
+        self.image = self.frames[self.state][int(self.frame_index % len(self.frames[self.state]))]
     def update(self, dt):
         
         self.input()
         self.move(dt)
+        self.animate(dt)
 
         

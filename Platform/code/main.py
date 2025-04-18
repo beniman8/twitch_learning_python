2004a5685832635f6d2b68fbb4c85be7ff3d0163
beniman8 <benik@live.ca>
2025-04-18 11:05:09 -0400

    wow

--- Platform/code/main.py
+++ Platform/code/main.py
@@ -1,4 +1,9 @@
 from settings import * 
+from sprites import *
+from groups import *
+from support import *
+from pimer import Timer 
+from random import randint
 
 class Game:
     def __init__(self):
@@ -7,12 +12,63 @@ class Game:
         pygame.display.set_caption('Platformer')
         self.clock = pygame.time.Clock()
         self.running = True
+        
+        
+            
+            
 
         # groups 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprite()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
+        
+        # load game
+        self.load_assets()
+        self.setup()
+        
+        
+        # timers 
+        self.bee_timer = Timer(500,func=self.create_bee,autostart=True,repeat=False)
+        
+    def create_bee(self):
+                Bee(self.bee_frames,(randint(300,600),randint(300,600)), self.all_sprites)
+        
+    def create_bullet(self,pos,direction):
+        x = pos[0] + direction * 34 if direction == 1 else pos[0] + direction *34 - self.bullet_surf.get_width()
+        Bullet(self.bullet_surf,(x,pos[1]),direction,(self.all_sprites,self.bullet_sprites))
+        Fire(self.fire_surf,pos,self.all_sprites,self.player)
+        
+    def load_assets(self):
+        # graphics 
+        self.player_frames = import_folder('images','player')
+        self.bullet_surf = import_image('images','gun','bullet')
+        self.fire_surf = import_image('images','gun','fire')
+        self.bee_frames = import_folder('images','enemies','bee')
+        self.worm_frames = import_folder('images','enemies','worm')
+        
+        #sounds 
+        
+        self.audio = audio_import('audio')
+        #self.audio['music'].play()
+        
+        
 
+    def setup(self):
+        tmx_map = load_pygame(join('data','maps','world.tmx'))
+        
+        for x,y,image in tmx_map.get_layer_by_name('Main').tiles():
+            Sprite((x*TILE_SIZE,y*TILE_SIZE), image,(self.all_sprites,self.collision_sprites))
 
+        for x,y,image in tmx_map.get_layer_by_name('Decoration').tiles():
+            Sprite((x*TILE_SIZE,y*TILE_SIZE), image,self.all_sprites)
+            
+            
+        for obj in tmx_map.get_layer_by_name("Entities"):
+            
+            if obj.name == "Player":
+                self.player = Player((obj.x,obj.y),self.all_sprites,self.collision_sprites,self.player_frames,self.create_bullet)
+        #Bee(self.bee_frames,(500,600),self.all_sprites)
+        Worm(self.worm_frames,(700,600),self.all_sprites)
     def run(self):
         while self.running:
             dt = self.clock.tick(FRAMERATE) / 1000 
@@ -22,11 +78,12 @@ class Game:
                     self.running = False 
             
             # update
+            self.bee_timer.update()
             self.all_sprites.update(dt)
 
             # draw 
             self.display_surface.fill(BG_COLOR)
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
         
         pygame.quit()
---
+++ Platform/code/quiz.py
@@ -0,0 +1,439 @@
+
+print("Hello" + str(5))
+# a) Hello5
+# b) Hello
+# c) Error
+
+
+print(2 * 3 + 4)
+# a) 10
+# b) 14
+# c) 12
+
+
+print(bool(0))
+# a) True
+# b) False
+# c) Error
+
+
+print(type([1, 2, 3]))
+# a) <class 'list'>
+# b) <class 'tuple'>
+# c) <class 'array'>
+
+
+x = [1, 2, 3]
+print(x[-1])
+# a) 3
+# b) 1
+# c) Error
+
+
+print("Python"[::-1])
+# a) nohtyP
+# b) Python
+# c) Error
+
+
+print(3 ** 2)
+# a) 6
+# b) 9
+# c) 8
+
+
+print(5 // 2)
+# a) 2
+# b) 2.5
+# c) 3
+
+x = "abc"
+print(x * 2)
+# a) abcabc
+# b) abc2
+# c) Error
+
+
+print(len({1, 2, 3, 2}))
+# a) 3
+# b) 4
+# c) 5
+
+# 11.
+# python
+# Copy
+# Edit
+# print(None == False)
+# a) True
+# b) False
+# c) Error
+
+# 12.
+# python
+# Copy
+# Edit
+# a = [1, 2, 3]
+# print(a[3])
+# a) 3
+# b) IndexError
+# c) None
+
+# 13.
+# python
+# Copy
+# Edit
+# print("3" + "4")
+# a) 7
+# b) 34
+# c) Error
+
+# 14.
+# python
+# Copy
+# Edit
+# print(int("10") + float("5.5"))
+# a) 15.5
+# b) 105.5
+# c) Error
+
+# 15.
+# python
+# Copy
+# Edit
+# def f(): return 1, 2
+# print(f())
+# a) (1, 2)
+# b) 1 2
+# c) [1, 2]
+
+# 16.
+# python
+# Copy
+# Edit
+# print(True + True)
+# a) 2
+# b) True
+# c) Error
+
+# 17.
+# python
+# Copy
+# Edit
+# print([1, 2] + [3, 4])
+# a) [1, 2, 3, 4]
+# b) [1, 2][3, 4]
+# c) Error
+
+# 18.
+# python
+# Copy
+# Edit
+# print(set("hello"))
+# a) {'h', 'e', 'l', 'o'}
+# b) ['h', 'e', 'l', 'o']
+# c) 'hello'
+
+# 19.
+# python
+# Copy
+# Edit
+# print(10 > 5 > 2)
+# a) True
+# b) False
+# c) Error
+
+# 20.
+# python
+# Copy
+# Edit
+# print(bool("False"))
+# a) False
+# b) True
+# c) Error
+
+# 21.
+# python
+# Copy
+# Edit
+# print(list(range(1, 5)))
+# a) [1, 2, 3, 4]
+# b) [1, 2, 3, 4, 5]
+# c) [1, 2, 3, 4, 5, 6]
+
+# 22.
+# python
+# Copy
+# Edit
+# x = [1, 2, 3]
+# print(x.pop())
+# a) 3
+# b) [1, 2]
+# c) Error
+
+# 23.
+# python
+# Copy
+# Edit
+# print(0.1 + 0.2 == 0.3)
+# a) True
+# b) False
+# c) Error
+
+# 24.
+# python
+# Copy
+# Edit
+# print('a' * 0)
+# a) 'a'
+# b) ''
+# c) Error
+
+# 25.
+# python
+# Copy
+# Edit
+# print(5 and 0)
+# a) 5
+# b) 0
+# c) True
+
+# 26.
+# python
+# Copy
+# Edit
+# print([i for i in range(3)])
+# a) [0, 1, 2]
+# b) [1, 2, 3]
+# c) [0, 1, 2, 3]
+
+# 27.
+# python
+# Copy
+# Edit
+# print(" ".isspace())
+# a) True
+# b) False
+# c) Error
+
+# 28.
+# python
+# Copy
+# Edit
+# print("hello".upper())
+# a) HELLO
+# b) Hello
+# c) error
+
+# 29.
+# python
+# Copy
+# Edit
+# print(list("abc"))
+# a) ['a', 'b', 'c']
+# b) abc
+# c) ['abc']
+
+# 30.
+# python
+# Copy
+# Edit
+# print(type({}))
+# a) <class 'dict'>
+# b) <class 'set'>
+# c) <class 'list'>
+
+# 31.
+# python
+# Copy
+# Edit
+# print("abc"[1])
+# a) a
+# b) b
+# c) c
+
+# 32.
+# python
+# Copy
+# Edit
+# print(bool([]))
+# a) False
+# b) True
+# c) Error
+
+# 33.
+# python
+# Copy
+# Edit
+# print((1, 2) + (3,))
+# a) (1, 2, 3)
+# b) (1, 2)(3,)
+# c) Error
+
+# 34.
+# python
+# Copy
+# Edit
+# x = [1, 2, 3]
+# x.append([4, 5])
+# print(x)
+# a) [1, 2, 3, 4, 5]
+# b) [1, 2, 3, [4, 5]]
+# c) Error
+
+# 35.
+# python
+# Copy
+# Edit
+# print(10 % 3)
+# a) 1
+# b) 3
+# c) 0
+
+# 36.
+# python
+# Copy
+# Edit
+# print(divmod(9, 2))
+# a) (4, 1)
+# b) 4.5
+# c) Error
+
+# 37.
+# python
+# Copy
+# Edit
+# print(type(lambda x: x))
+# a) <class 'function'>
+# b) <class 'lambda'>
+# c) <function>
+
+# 38.
+# python
+# Copy
+# Edit
+# x = {1, 2, 3}
+# x.add(4)
+# print(x)
+# a) {1, 2, 3, 4}
+# b) [1, 2, 3, 4]
+# c) Error
+
+# 39.
+# python
+# Copy
+# Edit
+# print("abc".find("b"))
+# a) 1
+# b) 2
+# c) -1
+
+# 40.
+# python
+# Copy
+# Edit
+# print("abc".replace("a", "z"))
+# a) zbc
+# b) abc
+# c) zabc
+
+# 41.
+# python
+# Copy
+# Edit
+# print(list((1, 2)))
+# a) [1, 2]
+# b) (1, 2)
+# c) Error
+
+# 42.
+# python
+# Copy
+# Edit
+# print(5 in [1, 2, 5])
+# a) True
+# b) False
+# c) Error
+
+# 43.
+# python
+# Copy
+# Edit
+# print(type(5 / 2))
+# a) <class 'float'>
+# b) <class 'int'>
+# c) <class 'double'>
+
+# 44.
+# python
+# Copy
+# Edit
+# print(min([5, 2, 9]))
+# a) 2
+# b) 5
+# c) 9
+
+# 45.
+# python
+# Copy
+# Edit
+# print("a" in "apple")
+# a) True
+# b) False
+# c) Error
+
+# 46.
+# python
+# Copy
+# Edit
+# print(sorted([3, 1, 2]))
+# a) [1, 2, 3]
+# b) [3, 2, 1]
+# c) Error
+
+# 47.
+# python
+# Copy
+# Edit
+# x = "42"
+# print(int(x) + 1)
+# a) 43
+# b) 421
+# c) Error
+
+# 48.
+# python
+# Copy
+# Edit
+# x = [1, 2, 3]
+# print(x[::-1])
+# a) [3, 2, 1]
+# b) [1, 2, 3]
+# c) Error
+
+# 49.
+# python
+# Copy
+# Edit
+# print(sum([1, 2, 3]))
+# a) 6
+# b) 123
+# c) Error
+
+# 50.
+# python
+# Copy
+# Edit
+# print("abc".title())
+# a) Abc
+# b) aBc
+# c) ABC
+
+
+
+
+rows = 5
+
+for i in range(0,rows):
+    for j in range(0,i+1):
+        print("*",end=' ')
+    print("\r")
